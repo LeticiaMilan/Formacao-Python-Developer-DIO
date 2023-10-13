b721681bb18e7603c1fbbd5cefe1aecdb32f80a8
Letícia Milan <leticiamilan.dev@gmail.com>
2023-10-13 17:46:44 -0300

    organizando: fundamentos

---
+++ 00 - Fundamentos/string_4.py
@@ -0,0 +1,24 @@
+nome = "Letícia"
+
+mensagem = f"""
+        Olá meu nome é {nome},
+    Eu estou aprendendo Python.
+            Essa mensagem tem diferentes recuos. 😊
+"""
+
+print(mensagem)
+
+
+print(
+    """
+    ============= MENU =============
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    ================================
+
+            Obrigado por usar nosso sistema!!!!
+"""
+)
---
+++ 00 - Fundamentos/variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Letícia"
+idade = 21
+
+nome, idade = "João Vitor", 21
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
