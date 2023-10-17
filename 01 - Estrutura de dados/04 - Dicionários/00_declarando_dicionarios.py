80d3da71b62ee48e30ac4ba42a80513473a454df
Letícia Milan <leticiamilan.dev@gmail.com>
2023-10-17 15:18:02 -0300

    estrutura de dados: declarando dicionarios

---
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -0,0 +1,8 @@
+pessoa = {"nome": "Letícia", "idade": 21}
+print(pessoa)
+
+pessoa = dict(nome="Letícia", idade=21)
+print(pessoa)
+
+pessoa["telefone"] = "3333-1234"  # {"nome": "Letícia", "idade": 21, "telefone": "3333-1234"}
+print(pessoa)
