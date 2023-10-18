9d88da16596edf2273a854fc9326bfd941c55e35
Letícia Milan <leticiamilan.dev@gmail.com>
2023-10-18 15:47:07 -0300

    estrutura de dados: acessando dados

---
+++ 01 - Estrutura de dados/04 - Dicionários/01_acessando_dados.py
@@ -0,0 +1,11 @@
+dados = {"nome": "Letícia", "idade": 21, "telefone": "3333-1234"}
+
+print(dados["nome"])  # "Letícia"
+print(dados["idade"])  # 21
+print(dados["telefone"])  # "3333-1234"
+
+dados["nome"] = "João Vitor"
+dados["idade"] = 22
+dados["telefone"] = "9988-1781"
+
+print(dados)  # {"nome": "João Vitor", "idade": 22, "telefone": "9988-1781"}
