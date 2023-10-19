48669b5638b32471f226adc9de351b8dd4666fb9
Letícia Milan <leticiamilan.dev@gmail.com>
2023-10-19 13:26:20 -0300

    estrutura de dados: dicionarios aninhados

---
+++ 01 - Estrutura de dados/04 - Dicionários/02_dicionarios_aninhados.py
@@ -0,0 +1,9 @@
+contatos = {
+    "leticiamilan.dev@gmail.com": {"nome": "Letícia", "telefone": "3333-2221"},
+    "joaovitor@gmail.com": {"nome": "João Vitor", "telefone": "3443-2121"},
+    "larissa@gmail.com": {"nome": "Larissa", "telefone": "3344-9871"},
+    "henrique@gmail.com": {"nome": "Henrique", "telefone": "3333-7766"},
+}
+
+telefone = contatos["joaovitor@gmail.com"]["telefone"]  # "3443-2121"
+print(telefone)
