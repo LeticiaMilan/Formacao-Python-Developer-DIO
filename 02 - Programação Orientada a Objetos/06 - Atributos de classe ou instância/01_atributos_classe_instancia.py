73ca96e21fd9d727f08fa884d046553073eb08ad
Letícia Milan <leticiamilan.dev@gmail.com>
2024-01-04 15:24:20 -0300

    feat: add oop module files

---
+++ 02 - Programação Orientada a Objetos/06 - Atributos de classe ou instância/01_atributos_classe_instancia.py
@@ -0,0 +1,23 @@
+class Estudante:
+    escola = "DIO"
+
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+    def __str__(self) -> str:
+        return f"{self.nome} - {self.matricula} - {self.escola}"
+
+
+def mostrar_valores(*objs):
+    for obj in objs:
+        print(obj)
+
+
+aluno_1 = Estudante("Leticia", 1)
+aluno_2 = Estudante("João Vitor", 2)
+mostrar_valores(aluno_1, aluno_2)
+
+Estudante.escola = "UNIP"
+aluno_3 = Estudante("Chappie", 3)
+mostrar_valores(aluno_1, aluno_2, aluno_3)
